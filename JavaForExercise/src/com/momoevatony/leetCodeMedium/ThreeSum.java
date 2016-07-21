5b720235625a3deaa76e8b29c620263b39ba7c20
Yi Tian <momoevatony@gmail.com>
2016-07-20 22:23:47 -0700

    7/20/2016 commit 2
    Three sum in progress

--- JavaForExercise/src/com/momoevatony/leetCodeMedium/ThreeSum.java
+++ JavaForExercise/src/com/momoevatony/leetCodeMedium/ThreeSum.java
@@ -1,6 +1,8 @@
 package com.momoevatony.leetCodeMedium;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class ThreeSum {
@@ -10,7 +12,23 @@ public class ThreeSum {
 	}
 	
 	static List<List<Integer>> threeSum(int[] nums) {
-        List
+        List<Integer> innerList = new ArrayList<Integer>();
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        HashMap <Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        
+        return list;
     }
+	
+	static List<List<Integer>> threeSum2(int[] nums){
+        List<Integer> innerList = new ArrayList<Integer>();
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+		
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+        	
+        }
+        
+        return list;
+	}
 
 }
