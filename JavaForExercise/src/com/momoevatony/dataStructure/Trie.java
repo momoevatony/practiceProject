e2d15d95a725713b4dc45e8fcbcb72fe0120f103
Yi Tian (PC Laptop) <momoevatony@gmail.com>
2016-12-02 17:14:25 -0800

    12/2/2016 commit 1
    Trie added

---
+++ JavaForExercise/src/com/momoevatony/dataStructure/Trie.java
@@ -0,0 +1,52 @@
+package com.momoevatony.dataStructure;
+
+public class Trie {
+	private final int MAX_SIZE = 26;
+	
+	private class TrieNode{
+		int num;
+		char ch;
+		boolean end;
+		
+		TrieNode [] child;
+		
+		public TrieNode(){
+			this.num = 1;
+			this.child = new TrieNode[MAX_SIZE];
+			this.end = false;
+		}
+	}
+	
+	public void insert(TrieNode node, String str){
+		if(str.length()==0 || str == null){return;}
+		
+		char[] letters = str.toCharArray();
+		for (int i=0;i<letters.length;i++){
+			node.ch=letters[i];
+			int position = letters[i]-'a';
+			if(node.child[position] == null){
+				node.child[position] = new TrieNode();
+			}else{
+				node.child[position].num++;
+			}
+			node = node.child[position];
+			if(i==letters.length-1){node.end=true;}
+		}
+	}
+	
+	public int findCound(TrieNode node, String str){
+		if(str.length()==0 || str == null){return 0;}
+		
+		char [] letters = str.toCharArray();
+		for(char ch : letters){
+			int position = ch-'a';
+			if(node.child[position] == null){
+				return 0;
+			}else{
+				node = node.child[position];
+			}
+		}
+		
+		return node.num;
+	}
+}
