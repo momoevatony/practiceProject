437a960c695c08c1c6e64916adc6c51c84406090
Yi Tian <momoevatony@gmail.com>
2017-03-08 23:35:43 -0800

    3.8.2017 commit 2
    Build Post Office 2 finished.

--- JavaForExercise/NineChapterBasicAlgorithm/course4/BuildPostOffice2.java
+++ JavaForExercise/NineChapterBasicAlgorithm/course4/BuildPostOffice2.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 
 public class BuildPostOffice2 {
 	
-	//Note: 3.8.2017 needs to debug, how to determine all houses could be reached???
+	//accepted 3.8.2017 7325ms
 	public int shortestDistance(int[][] grid) {
         // Write your code here
 		if(grid.length == 0 || grid[0].length==0){return -1;}
@@ -23,21 +23,18 @@ public class BuildPostOffice2 {
 				}
 			}
 		}
-		System.out.println("Find "+candidates+" available points");
 		int[][] distances = new int[grid.length][grid[0].length];
+		int[][] reached = new int[grid.length][grid[0].length];
 		//For each house, use BFS to update the distance map
-		boolean canReach = false;
 		for(Point curr: houses){
-			canReach = BFS(grid, distances, curr, candidates);
+			BFS(grid, distances, curr, reached);
 		}
-		if(canReach){return findMin(distances);}
-		return -1;
+		int min = findMin(distances, reached, houses.size());
+		return min==Integer.MAX_VALUE?-1:min;
     }
 	
-	private boolean BFS(int[][]grid, int[][] distances, Point start, int candidates){
-	    System.out.println("Analyzing "+start.x+" "+start.y);
+	private void BFS(int[][]grid, int[][] distances, Point start, int[][]reached){
 		boolean[][] visited = new boolean[grid.length][grid[0].length];
-		int visitedCandidates = 0;
 		int distance = 0;
 		Queue<Point> queue = new LinkedList<Point>();
 		queue.offer(start);
@@ -56,26 +53,24 @@ public class BuildPostOffice2 {
 					queue.offer(next);
 					visited[next.x][next.y] = true;
 					distances[next.x][next.y] += distance;
-					visitedCandidates++;
-					System.out.println("To "+next.x+" "+next.y+" is "+distance);
+					reached[next.x][next.y]++;
 				}
 			}
 		}
-		return visitedCandidates == candidates;
 	}
 	
-	private int findMin(int[][] distances){
+	private int findMin(int[][] distances, int[][]reached, int houses){
 		int min = Integer.MAX_VALUE;
 		for(int i = 0; i<distances.length; i++){
 			for(int j = 0; j<distances[0].length; j++){
-				if(distances[i][j] > 0){min = min<distances[i][j]?min:distances[i][j];}
+				if(distances[i][j] > 0 && reached[i][j] == houses){min = min<distances[i][j]?min:distances[i][j];}
 			}
 		}
 		return min;
 	}
 	
 	private boolean validPoint(int[][]grid, Point curr){
-		return (curr.x>=0 && curr.x<grid.length && curr.y>=0 && curr.y<grid[0].length && grid[curr.x][curr.y] != 2);
+		return (curr.x>=0 && curr.x<grid.length && curr.y>=0 && curr.y<grid[0].length && grid[curr.x][curr.y] ==0);
 	}
 	
 	
